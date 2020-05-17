d91c51c2eb0b62da96480ca282978267d232fcf6
clara adelia <claraadelia12@outlook.com>
2020-05-18 00:48:43 +0700

    coba login

--- middleware/auth.js
+++ middleware/auth.js
@@ -45,3 +45,54 @@ exports.registrasi = function(req,res) {
     })
 }
 
+
+//controller untuk login
+exports.login = function(req,res){
+    var post = {
+        password:req.body.password,
+        email:req.body.email,
+
+    }
+
+    var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
+    var table = ["t_user", "password",md5(post, password), "email", post.email];
+
+    query = mysql.format(query,table);
+    connection.query(query, function(error, rows){
+        if(error){
+            console.log(error);
+        }else{
+            if(rows == 1){
+                var token = jwt.sign({rows}, config,secret,{
+                    expireIn:1440
+                })
+                id_user = rows[0].id;
+
+                var data = {
+                    id_user = id_user,
+                    akses_token : token,
+                    ip_address: ip.address()
+                }
+
+                var query = "INSERT INTO ?? SET ?";
+                var table = [akses_token];
+
+                query = mysql.format(query, table)
+                connection.query(query,data, function(error, rows){
+                    if(error){
+                        console.log(error);
+                    } else {
+                        res.json({
+                            success: true,
+                            message: "Token JWT Tergenerate!",
+                            token:token,
+                            currUser: data.id_user
+                        });
+                    }
+                });
+            }else {
+                res.json({"Error": true, "Message":"Email atau password salah!"});
+            }
+        }
+    });
+}
